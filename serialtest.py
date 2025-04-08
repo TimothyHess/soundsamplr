350c3b290b7c4ef10936d0010b3ccca828b8705d
Timothy Hess <thes0002@student.monash.edu>
2025-04-08 10:45:19 +1000

    Create serialtest.py

---
+++ serialtest.py
@@ -0,0 +1,8 @@
+import serial
+import serial.tools.list_ports
+import time
+ser= serial.Serial("COM3",115200)
+while(1):
+    data = ser.read().decode()
+    print(data, end="")
+ #   time.sleep(0.1)
