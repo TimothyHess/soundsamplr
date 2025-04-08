219686cdfce253ce5cd62e5149ca9685aa6dcb96
Timothy Hess <thes0002@student.monash.edu>
2025-04-08 11:25:31 +1000

    Folder

--- serialtest.py
+++ serialtest.py
@@ -3,6 +3,8 @@ import serial.tools.list_ports
 import time
 ser= serial.Serial("COM3",115200)
 while(1):
+    print(1)
     data = ser.read().decode()
     print(data, end="")
  #   time.sleep(0.1)
+    
